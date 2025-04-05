231dff5151a6d16b067f84d712fedbf6d1dc074a
Aryan Prasad <aryanprasad3195@gmail.com>
2025-04-05 22:52:35 +0530

    Add files via upload

---
+++ sorting1darraydynamically using ptr.cpp
@@ -0,0 +1,6 @@
+//wap to read n-elements in a one-d array and sort them using pointer notation (without using array) ie pointer notation dynamically lena hai
+#include <iostream>
+using namespace std;
+int main(){
+	
+}
