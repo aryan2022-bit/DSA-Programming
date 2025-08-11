06d86f89920a5449befa2c4ae29db5233a6be31b
Aryan Prasad <aryanprasad3195@gmail.com>
2025-08-12 00:34:35 +0530

    Create Valid Parentheses.cpp

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(int i = 0; i < s.length(); i++){
+            char ch =  s[i];
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }else if(ch == ')' || ch == '}' || ch == ']'){
+                if(st.empty()) return false;
+                
+                if(st.top() == '(' && ch == ')' ||
+                   st.top() == '{' && ch == '}' ||
+                   st.top() == '[' && ch == ']')
+                { 
+                st.pop();
+                }else{
+                    return false;
+                    }
+            }
+        }
+        return st.empty();
+    }
+};
