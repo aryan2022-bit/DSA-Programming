02eb94477e0f91b7b13b99f3d837572e5cef2839
Aryan Prasad <aryanprasad3195@gmail.com>
2025-09-15 09:56:36 +0530

    Create Two Sum II - Input Array Is Sorted (O(n) Space Hash Map Approach).cpp

---
+++ Two Sum II - Input Array Is Sorted (O(n) Space Hash Map Approach).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        unordered_map<int, int> map;
+
+        for(int i = 0; i < numbers.size(); i++){
+            int complement = target - numbers[i];
+
+            if(map.find(complement) != map.end()){
+                return {map[complement] + 1, i + 1};
+            }else{
+                map[numbers[i]] = i;
+            }
+        }
+        return {};
+    }
+};
