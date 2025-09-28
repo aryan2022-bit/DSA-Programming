3f35befa2b0f7cc3cf88abcad395931d9aedbeac
Aryan Prasad <aryanprasad3195@gmail.com>
2025-09-28 11:47:00 +0530

    Create Symmetric Tree (Iterative Approach).cpp

---
+++ Symmetric Tree (Iterative Approach).cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        queue<TreeNode*> mirror;
+        mirror.push(root -> left);
+        mirror.push(root -> right);
+
+        while(!mirror.empty()){
+            TreeNode* a = mirror.front();
+            mirror.pop();
+            TreeNode* b = mirror.front();
+            mirror.pop();
+
+            if(a == nullptr && b == nullptr) continue;
+            if(a == nullptr || b == nullptr) return false;
+            if(a -> val != b -> val) return false;
+
+            mirror.push(a -> left);
+            mirror.push(b -> right);
+            mirror.push(a -> right);
+            mirror.push(b -> left);
+        }
+        return true;
+    }
+};
