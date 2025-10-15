60b448a2341efa1d47f0144f6ea972257dc572aa
Aryan Prasad <aryanprasad3195@gmail.com>
2025-10-15 11:46:06 +0530

    Create Linked List Cycle (Hash Set Approach).cpp

---
+++ Linked List Cycle (Hash Set Approach).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*> visited;
+        ListNode* curr = head;
+
+        while(curr != nullptr){
+            if(visited.find(curr) != visited.end()){
+                return true;
+            }
+
+            visited.insert(curr);
+            curr = curr -> next;
+        }
+        return false;
+    }
+};
