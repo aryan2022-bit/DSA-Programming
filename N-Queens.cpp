826263a3b99ab548f6bcdf1f92524ed6051a6af8
Aryan Prasad <aryanprasad3195@gmail.com>
2025-12-10 03:21:40 +0530

    Create N-Queens.cpp

---
+++ N-Queens.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> result;
+        vector<string> board(n, string(n, '.'));
+
+        unordered_set<int> cols;
+        unordered_set<int> diag1;
+        unordered_set<int> diag2;
+
+        dfs(0, n, result, board, cols, diag1, diag2);
+        return result;
+    }
+    void dfs(int row, int n, vector<vector<string>>& result, vector<string>& board, unordered_set<int>& cols, unordered_set<int>& diag1, unordered_set<int>& diag2){
+        // Base Case
+        if(row == n){
+            result.push_back(board);
+            return;
+        }
+
+        for(int col = 0; col < n; col++){
+            if(cols.count(col) || diag1.count(row - col) || diag2.count(row + col)) continue;
+
+            board[row][col] = 'Q';
+            cols.insert(col);
+            diag1.insert(row - col);
+            diag2.insert(row + col);
+
+            dfs(row + 1, n, result, board, cols, diag1, diag2);
+
+            board[row][col] = '.';
+            cols.erase(col);
+            diag1.erase(row - col);
+            diag2.erase(row + col);
+        }
+    }
+};
