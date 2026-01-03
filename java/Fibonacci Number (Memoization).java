a942969520a36775092ed601c8c8a57ef2e26ed6
Aryan Prasad <aryanprasad3195@gmail.com>
2026-01-04 01:11:46 +0530

    Create Fibonacci Number (Memoization).java

---
+++ java/Fibonacci Number (Memoization).java
@@ -0,0 +1,16 @@
+class Solution {
+    int[] dp;
+    public int fib(int n) {
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return solve(n);
+    }
+
+    int solve(int n){
+        if(n <= 1) return n;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return solve(n - 1) + solve(n - 2);
+    }
+}
